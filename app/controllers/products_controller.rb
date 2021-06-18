04424fc96d14eb092515d51fc3df7335cb73a8cf
“KarxGonzo” <KarxGonzo@gmail.com>
2021-06-17 20:16:14 -0500

    Updated Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show 
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    render json: product.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:image_url],
+      description: params[:input_description]
+    )
+    product.save
+    render json: product.as_json
+  end
 end
